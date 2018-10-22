61dffddf4d31911746d412017d182c044311f49a
Felipe <felipe.florianopolis@gmail.com>
2018-10-22 14:58:57 -0300

    Starting de snippetify project

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(3000);
+
+npm install eslint eslint-plugin-import eslint-config-airbnb-base -D
