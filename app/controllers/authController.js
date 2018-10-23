dc4ab89b4c09a6554b1ed15988c9a3f15db87ad6
Felipe <felipe.florianopolis@gmail.com>
2018-10-22 23:47:51 -0300

    User register module and bcrypt lib to encrypt the user's password

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const { User } = require('../models');
+
 module.exports = {
   signin(req, res) {
     return res.render('auth/signin');
@@ -5,5 +8,18 @@ module.exports = {
 
   signup(req, res) {
     return res.render('auth/signup');
+  },
+
+  async register(req, res) {
+    const { email } = req.body;
+
+    if (await User.findOne({ where: { email } })) {
+      return res.redirect('back');
+    }
+
+    const password = await bcrypt.hash(req.body.password, 5);
+
+    await User.create({ ...req.body, password });
+    return res.redirect('/');
   },
 };
